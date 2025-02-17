94576cd379a49cc15a034066d199f2475808df59
imanbek <taalaybekovimanbek@gmail.com>
2025-02-17 18:01:28 +0600

    Объявления и голосования

---
+++ app/Models/Poll.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Poll extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'type',
+        'residential_complex_id',
+        'building_number',
+        'start_date',
+        'end_date'
+    ];
+
+    public function options()
+    {
+        return $this->hasMany(PollOption::class);
+    }
+
+    public function votes()
+    {
+        return $this->hasMany(PollVote::class);
+    }
+}
---
+++ database/migrations/2025_02_17_114531_create_polls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('polls', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('type', ['complex', 'building']);
+            $table->foreignId('residential_complex_id')->nullable()->constrained();
+            $table->string('building_number')->nullable();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('polls');
+    }
+};
